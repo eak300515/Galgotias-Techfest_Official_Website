2fb59bd288c0c4a1b4c7b094c4e8945d8297d678
eak300515 <eakadityakumar15@gmail.com>
2024-11-23 14:27:26 +0530

    Frontend and Backend Issues fixed

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -62,7 +62,7 @@ const Header = () => {
                 onClick={handleClick}
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
                   item.onlyMobile ? "lg:hidden" : ""
-                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                } px-6 py-4 md:py-4 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
                   location.hash === item.url
                     ? "z-2 lg:text-n-1"
                     : "lg:text-n-1/50"
