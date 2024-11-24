0e75b8e622b768d7662ddf597c39cf7526059af5
eak300515 <eakadityakumar15@gmail.com>
2024-11-24 11:28:56 +0530

    Backend Hosted

--- Client/src/RegistrationForm.jsx
+++ Client/src/RegistrationForm.jsx
@@ -59,7 +59,7 @@ const RegistrationForm = () => {
     // If no errors, send data to backend
     if (Object.keys(validationErrors).length === 0) {
       try {
-        const response = await fetch("https://ec2-16-170-234-226.eu-north-1.compute.amazonaws.com//api/registrations", {
+        const response = await fetch("https://galgotias-techfest-official-website-5rm4.onrender.com/api/registrations", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
