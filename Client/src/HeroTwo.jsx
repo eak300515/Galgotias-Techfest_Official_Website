397c56458c1beda3bf6982283716c999511df34c
eak300515 <eakadityakumar15@gmail.com>
2025-01-07 13:50:37 +0530

    Header Redesign

--- Client/src/HeroTwo.jsx
+++ Client/src/HeroTwo.jsx
@@ -16,7 +16,7 @@ const HeroTwo = () => {
       <div className="hero-two-content">
         <h1 className="hero-two-title">
           Experience the <span className="highlight">Future of Innovation</span> at
-          <span className="highlight-tech"> Galgotias TechFest 2025</span>
+          <span className="highlight-tech"> Galgotias TechFest 2025</span>24th - 26th March 2025
         </h1>
         <div className="hero-two-buttons">
           <button class="btn">Event Brochure</button>
