8e611b3b4031bc9bdd3d7704f82a2735a418934c
eak300515 <eakadityakumar15@gmail.com>
2025-01-07 15:46:53 +0530

    Name Changes

--- Client/src/HeroTwo.jsx
+++ Client/src/HeroTwo.jsx
@@ -16,7 +16,7 @@ const HeroTwo = () => {
       <div className="hero-two-content">
         <h1 className="hero-two-title">
           Experience the <span className="highlight">Future of Innovation</span> at
-          <span className="highlight-tech"> Galgotias TechFest 2025</span>
+          <span className="highlight-tech"> Galgotias TechVerse 2025</span>
         </h1>
         <div className="hero-two-buttons">
           <button class="btn">Event Brochure</button>
