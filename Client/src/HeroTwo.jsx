3cbdd255efc6cf368ed8e06b58e06c07ebec441c
eak300515 <eakadityakumar15@gmail.com>
2025-01-07 16:02:34 +0530

    Name Changes

--- Client/src/HeroTwo.jsx
+++ Client/src/HeroTwo.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import './HeroTwo.css';
 
 const HeroTwo = () => {
+  const handleScrollToForm = () => {
+    const element = document.getElementById("registration-form");
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+
   return (
     <section className="hero-two-bg min-h-screen flex items-center">
       {/* Loading Animation */}
@@ -20,7 +28,7 @@ const HeroTwo = () => {
         </h1>
         <div className="hero-two-buttons">
           <button class="btn">Event Brochure</button>
-          <button class="btn">Register Now</button>
+          <button class="btn" onClick={handleScrollToForm}>Register Now</button>
         </div>
         <h1 className="hero-two-title"><span className="highlight"> 24th - 26th March</span></h1>
       </div>
