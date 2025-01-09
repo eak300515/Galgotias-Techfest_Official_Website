e878294271d7b44547f43d715e69b2be0bf3d9e6
eak300515 <eakadityakumar15@gmail.com>
2025-01-09 14:23:41 +0530

    Finalizing GTC Logos

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
-import GalgotiasLogo from "https://res.cloudinary.com/di1mfyjus/image/upload/v1736408583/vnbs1td7ygyjmesoorv1.png";
+import GalgotiasLogo from "../assets/white logo.png";
 
 const Header = () => {
   const [openNavigation, setOpenNavigation] = useState(false);
